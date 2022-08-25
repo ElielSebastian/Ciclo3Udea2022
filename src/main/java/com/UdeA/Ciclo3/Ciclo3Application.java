3ad4db60d0303c5c001a22aff5283a5b55df24da
ElielSebastian <111058146+ElielSebastian@users.noreply.github.com>
2022-08-24 22:41:01 -0500

    One Day

--- src/main/java/com/UdeA/Ciclo3/Ciclo3Application.java
+++ src/main/java/com/UdeA/Ciclo3/Ciclo3Application.java
@@ -1,13 +1,27 @@
 package com.UdeA.Ciclo3;
 
+import com.UdeA.Ciclo3.modelos.Empresa;
+import org.hibernate.annotations.GeneratorType;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+@RestController
+@SpringBootApplication (exclude = {SecurityAutoConfiguration.class})
 public class Ciclo3Application {
+
+	@GetMapping("/test")
+	public String test(){
+		Empresa emp = new Empresa("BikeHouse", "Calle 67 #53 - 108", "2198332", "890980040-8");
+		emp.setNombre("BikeHouseProject");
+		return emp.getNombre();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Ciclo3Application.class, args);
+
 	}
 
 }
