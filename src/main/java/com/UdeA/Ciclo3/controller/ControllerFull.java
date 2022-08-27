ed84267ff2283880dbd8eede2c96b7ffc617b351
ElielSebastian <111058146+ElielSebastian@users.noreply.github.com>
2022-08-27 12:19:33 -0500

    commit 2.0

--- src/main/java/com/UdeA/Ciclo3/controller/ControllerFull.java
+++ src/main/java/com/UdeA/Ciclo3/controller/ControllerFull.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -37,6 +38,15 @@ public class ControllerFull {
             return  "redirect:/verEmpresas";
         }
         return "redirect:/AgregarEmpresa";
+    }
+
+    @GetMapping("/EditarEmpresa")
+    public String editarEmpresa(Model model, @PathVariable Integer id){
+        Empresa emp=empresaService.getEmpresaById(id);
+        model.addAttribute("emp",emp);
+        return "editarEmpresa";
+
+
     }
 
     
