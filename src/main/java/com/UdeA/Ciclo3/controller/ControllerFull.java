332a97f3a73b30cab19636d3b4db921320f270f6
ElielSebastian <111058146+ElielSebastian@users.noreply.github.com>
2022-08-30 11:57:34 -0500

    2.0

--- src/main/java/com/UdeA/Ciclo3/controller/ControllerFull.java
+++ src/main/java/com/UdeA/Ciclo3/controller/ControllerFull.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -35,8 +36,10 @@ public class ControllerFull {
     @PostMapping("/GuardarEmpresa")
     public String guardarEmpresa(Empresa emp, RedirectAttributes redirectAttributes){
         if(empresaService.saveOrUpdateEmpresa(emp)==true){
+            redirectAttributes.addFlashAttribute("mensaje","saveOK");
             return  "redirect:/verEmpresas";
         }
+        redirectAttributes.addFlashAttribute("mensaje","saveError");
         return "redirect:/AgregarEmpresa";
     }
 
@@ -49,10 +52,12 @@ public class ControllerFull {
     }
 
     @PostMapping("/ActualizarEmpresa")
-    public String updateEmpresa(Empresa emp){
-        if(empresaService.saveOrUpdateEmpresa(emp)==true){
+    public String updateEmpresa(@ModelAttribute("emp") Empresa emp, RedirectAttributes redirectAttributes){
+        if(empresaService.saveOrUpdateEmpresa(emp)){
+            redirectAttributes.addFlashAttribute("mensaje","saveOK");
             return  "redirect:/verEmpresas";
         }
+        redirectAttributes.addFlashAttribute("mensaje","updateError");
         return "redirect:/EditarEmpresa";
     
     }
